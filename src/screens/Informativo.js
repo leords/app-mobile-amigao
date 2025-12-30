5c5707281b4774784838426eac0ce4ec610a54a2
leords <leo_ro.drigues@hotmail.com>
2025-12-30 18:23:11 -0300

    descrição de versão

--- src/screens/Informativo.js
+++ src/screens/Informativo.js
@@ -52,7 +52,7 @@ export default function Informativo() {
           <Text style={estilos.textoBotaoSair}>Sair da minha conta</Text>
         </TouchableOpacity>
         <Text style={{ marginTop: 20, fontWeight: 200, fontSize: 12 }}>
-          Versão do aplicativo: 2.1.1
+          Versão do aplicativo: 2.1.2 - @2026
         </Text>
       </View>
     </View>
